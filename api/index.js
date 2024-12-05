c3550bbfb06be5e71454adf79122d354baba7939
Lucas Minoro <lucas.minoro@gmail.com>
2024-12-05 15:52:51 -0300

    só na reza

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://blog:WQEJmthwXoiqtyGx@cluster0.4kame.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
